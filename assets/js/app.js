5e4f7e7c597821d1c4ab8b0dd52b3fcb921831ee
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-12-03 19:32:30 +0100

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
-import Reveal from '/node_modules/reveal.js/dist/reveal.esm.js';
-import Markdown from '/node_modules/reveal.js/plugin/markdown/markdown.esm.js';
-import * as RevealMath from "/node_modules/reveal.js/plugin/math/katex.js";
-import "/node_modules/reveal.js/plugin//highlight/highlight.js";
+import Reveal from '/../../node_modules/reveal.js/dist/reveal.esm.js';
+import Markdown from '/../../node_modules/reveal.js/plugin/markdown/markdown.esm.js';
+import * as RevealMath from "/../../node_modules/reveal.js/plugin/math/katex.js";
+import "/../../node_modules/reveal.js/plugin//highlight/highlight.js";
 
 let deck = new Reveal({
     plugins: [RevealHighlight,Markdown,RevealMath.KaTeX],
